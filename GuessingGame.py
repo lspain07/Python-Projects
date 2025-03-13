12e33a8bddae437961831796a63e6ab3a67801b6
lspain07 <lukasrspain@gmail.com>
2025-03-13 13:32:18 -0500

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,7 +1,1 @@
-import random
-guess = int(input("Guess a number from 1 to 10: "))
-answer = random.randint(1,10)
-if guess == answer:
-    print("You got it right!")
-else:
-    print("You got it wrong! The answer was " + str(answer) + "!")
+
