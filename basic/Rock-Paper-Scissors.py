6a69f9e9bbf40b299deba7f08d9d2d642428e475
lspain07 <lukasrspain@gmail.com>
2025-03-17 10:56:26 -0500

    Finish Rock-Paper-Scissors.py

--- basic/Rock-Paper-Scissors.py
+++ basic/Rock-Paper-Scissors.py
@@ -1,1 +1,21 @@
 import random
+choice = str(input("Rock, Paper, or Scissors? "))
+comp = random.randint(1,3) #1 is rock, 2 is paper, 3 is scissors
+if choice == "Rock" or "rock" and comp == 2:
+  print("Paper! You lose!")
+elif choice == "Rock" or "rock" and comp == 1:
+  print("Rock! Go again!")
+elif choice == "Rock" or "rock" and comp == 3:
+  print("Scissors! You win!")
+if choice == "Paper" or "paper" and comp == 3:
+  print("Scissors! You lose!")
+elif choice == "Paper" or "paper" and comp == 1:
+  print("Rock! You win!")
+elif choice == "Paper" or "paper" and comp == 2:
+  print("Paper! Go again!")
+if choice == "Scissors" or "scissors" and comp == 1:
+  print("Rock! You lose!")
+elif choice == "Scissors" or "scissors" and comp == 2:
+  print("Paper! You win!")
+elif choice == "Scissors" or "scissors" and comp == 3:
+  print("Scissors! Go again!")
