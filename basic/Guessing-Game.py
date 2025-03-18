d4ea3e1e6f9ee1d4e076b53dc6a3cc208fab0f3a
lspain07 <lukasrspain@gmail.com>
2025-03-18 13:34:33 -0500

    Fleshing out Guessing-Game.py

--- basic/Guessing-Game.py
+++ basic/Guessing-Game.py
@@ -1,10 +1,15 @@
-import random #imports the random module's functions into the file
-guess = int(input("Guess a number from 1 to 10: ")) #declares the "guess" variable as an integer meant to be entered by the user
-answer = random.randint(1,10) #declares the "answer" variable and uses the randint function of the random module to assign a value from 1 to 10 to the answer variable
-if guess == answer:
-    print("You got it right!") 
-elif guess > 10 or guess < 1:
-    print("You idiot! That's not in the range!")
-else:
-    print("You got it wrong! The answer was " + str(answer) + "!")
-#the integer value of the guess and answer variables must be equal for the file to print the first message
+def game():
+    import random #imports the random module's functions into the file
+    guess = int(input("Guess a number from 1 to 10: "))
+    answer = random.randint(1,10)
+    if guess == answer:
+        print("You got it right!")
+    elif guess > 10 or guess < 1:
+        print("You idiot! That's not in the range!")
+    else:
+        print("You got it wrong! The answer was " + str(answer) + "!")
+while True:
+    game()
+    user_input = input("Try again? (y/n): ")
+    if user_input.lower() != "y":
+        break
