32e0ce9740e51dddaa5859003ba2825881e4b7f7
lspain07 <lukasrspain@gmail.com>
2025-03-19 09:08:57 -0500

    Update Rock-Paper-Scissors.py

--- basic/Rock-Paper-Scissors.py
+++ basic/Rock-Paper-Scissors.py
@@ -1,7 +1,5 @@
 import random
 choice = str(input("All Caps! ROCK, PAPER, or SCISSORS? "))
 comp = random.randint(1,3) #1 is rock, 2 is paper, 3 is scissors
-#Is it trying to run different values of the comp variable?
-#Edit to make sure it only produces one output
-#Do I need to return the value of the random integer value for comp?
-#Does the value of comp need to be some kind of function?
+def main():
+  
