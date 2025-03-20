aa552caf172dcc88c9381cb6a61a5d633f2f95c1
lspain07 <lukasrspain@gmail.com>
2025-03-20 12:43:03 -0500

    Update Guessing-Game.py

--- basic/Guessing-Game.py
+++ basic/Guessing-Game.py
@@ -1,13 +1,19 @@
+import random
 def game():
-    import random
-    guess = int(input("Guess a number from 1 to 10: "))
-    answer = random.randint(1,10)
-    if guess == answer:
-        print("You got it right!")
-    elif guess > 10 or guess < 1:
-        print("You idiot! That's not in the range!")
-    else:
-        print("You got it wrong! The answer was " + str(answer) + "!")
+    while True:
+        try:
+            guess = int(input("Guess a number from 1 to 10: "))
+            answer = random.randint(1,10)
+            if guess == answer:
+                print("You got it right!")
+                break
+            elif guess > 10 or guess < 1:
+                print("You idiot! That's not in the range!")
+            else:
+                print("You got it wrong! The answer was " + str(answer) + "!")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 while True:
     game()
     query = input("Keep going? (y/n): ")
